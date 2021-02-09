ae45dacad7238acac14f5478d52313232ac8cdf5
andrey-dum <duminskiy@gmail.com>
2021-02-09 22:29:28 +0200

    TodoList. todo. fetchTodos. Pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 import UserList from './components/UserList';
 
 function App() {
   return (
     <div className="app">
       <UserList />
+      <hr />
+      <TodoList />
     </div>
   );
 }
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react'
 import { useActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/useTypedSelector';
-import { fetchUsers } from '../store/action-creators/user';
+
 
 const UserList: React.FC = (): React.ReactElement => {
     const {users, loading, error} = useTypedSelector(state => state.user)
