c9ed9c6af869c7028667ba1f25a5566cab8a10fa
andrey-dum <duminskiy@gmail.com>
2021-02-09 20:52:57 +0200

    action-creators. thunk fetchUsers

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {useDispatch} from 'react-redux'
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { fetchUsers } from '../store/action-creators/user';
 
 const UserList: React.FC = (): React.ReactElement => {
+    const dispatch = useDispatch()
     const {users, loading, error} = useTypedSelector(state => state.user)
 
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, []);
+
+    console.log(users);
+
+    if(loading) {
+        return <h1>LOADING...</h1>
+    }
+    if(error) {
+        return <h1>{error}</h1>
+    }
+    
 
     return (
         <div>
-            UserList
+            {users.map(u => 
+                <div key={u.id}>
+                    {u.name}
+                </div>)}
         </div>
     )
 }
---
+++ src/store/action-creators/user.ts
@@ -0,0 +1,21 @@
+import { Dispatch } from 'react';
+import { UserAction, UserActionsTypes } from './../../types/user';
+import axios from 'axios';
+
+
+export const fetchUsers = () => {
+    return async (dispatch: Dispatch<UserAction>) => {
+        try {
+            dispatch({type: UserActionsTypes.FETCH_USERS})
+
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+            setTimeout(() => {
+                dispatch({type: UserActionsTypes.FETCH_USERS_SUCCESS, payload: response.data})
+            }, 500)
+            
+
+        } catch (error) {
+            dispatch({type: UserActionsTypes.FETCH_USERS_ERROR, payload: 'Произошла ошибка при загрузке пользователя!'})
+        }
+    }
+}
